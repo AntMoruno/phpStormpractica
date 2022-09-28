43660b068cd873256cc8c6dfd166de7d09940d84
Manuel <manumoruno@gmail.com>
2022-09-28 10:50:51 +0200

    Se ha añadido un párrafo en la línea 13

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- Antonio Carlos Moruno Gómez -->
+
+<html lang="es">
+    <head>
+        <title>Ejercicio Factoriales</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Antonio Moruno <antoniocarlosmorunogomez.guadalupe@alumnado.fundacionloyola.net>">
+    </head>
+    <body>
+        <?php
+            echo "<p> Integración GitHub con phpStorm </p>"
+        ?>
+    </body>
+</html>
